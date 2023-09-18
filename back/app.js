f15c611e7296f93bcda9d536e233d7606e8c57e8
BRANDONGRADOS <bgradosalvarez@gmail.com>
2023-09-18 14:15:22 -0500

    conección a la base de datos mongo db

---
+++ back/app.js
@@ -0,0 +1,23 @@
+//importar
+var express = require('express');
+var mongoose = require('mongoose');
+var port = process.env.port || 4201;
+//inicializar
+var app = express();
+
+mongoose.connect=('mongodb://127.0.0.1:27017/tortasmarife',(err,res)=>{
+if(err){
+    console.log(err);
+}
+else{
+    //ponerlo en escucha
+    app.listen(port,function(){
+    console.log('servidor corriendo como tortuguita ' +port);
+});
+}
+});
+
+
+
+//exportar
+module.exports = app;
